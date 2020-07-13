623118b21231993d5fa2091d7a584e55f8e96030
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-13 19:02:08 -0300

    add retrofit

--- app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.LoginViewModel
@@ -53,7 +55,16 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     /**
      * Observa ViewModel
      */
-    private fun observe() {}
+    private fun observe() {
+        mViewModel.login.observe(this, Observer {
+            if (it.sucess()){
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                val message = it.failure()
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 
     /**
      * Autentica usuário
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,19 +2,51 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
+import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.repository.PersonRepository
+import com.example.tasks.service.repository.local.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPersonRepository = PersonRepository(application)
+    private val mSharedPreferences = SecurityPreferences(application)
+
+    private val mLogin = MutableLiveData<ValidationListener>()
+    var login: LiveData<ValidationListener> = mLogin
 
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
+        mPersonRepository.login(email, password, object : APIListener{
+            override fun onSucess(model: HeaderModel) {
+
+                mSharedPreferences.store(TaskConstants.SHARED.TOKEN_KEY, model.token)
+                mSharedPreferences.store(TaskConstants.SHARED.PERSON_KEY, model.personKey)
+                mSharedPreferences.store(TaskConstants.SHARED.PERSON_NAME, model.name)
+
+                mLogin.value = ValidationListener()
+
+            }
+
+            override fun onFailure(str: String) {
+                mLogin.value = ValidationListener(str)
+            }
+
+        })
     }
 
     /**
      * Verifica se usuário está logado
      */
     fun verifyLoggedUser() {
+
+
     }
 
 }
